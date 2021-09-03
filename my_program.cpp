761449cb073b45d02eec2d5061ec3f116adbe66c
Juan Felipe Bastidas Salgado <Juan.felipe@campusucc.edu.co>
2021-09-02 21:49:47 -0500

    01. mi primer commit en git para el programa

---
+++ my_program.cpp
@@ -0,0 +1,22 @@
+<stdio.h>
+<math.h>
+<getch.h>
+
+#workpace 'curso devops'
+
+static int main[object ...] {
+
+	int ivalor = 0;
+	int icontador = 0;
+
+	cin << 'ingrese un valor para calcular una suma';
+
+	for (i =1;i <= ivalor; i++ {
+	icontador+=i;
+	}
+	cout>> 'el resultado es: ' +%integer,icontador;
+
+	getch();
+
+	exit 1;
+}		
