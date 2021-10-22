71501c886af1dfdceaef609ed5084d33e0dfcad5
Juan Felipe Bastidas Salgado <Juan.felipe@campusucc.edu.co>
2021-10-21 20:24:59 -0500

    se separo

--- geometrica.cpp
+++ geometrica.cpp
@@ -76,8 +76,18 @@ int main() {
 	    vf_area = vi_base * vi_base;
             break;
 	}
+	    cout << "\n";
+            int argc
+            char const *argv[]
+        {
+            double numero= vf_area;
+            double RaizCuadrada = sqrt(vf_area);
+            cout << "la raiz cuadrada es => " << numero << RaizCuadrada << ;
+	    cout << "La area es => " << vf_area << endl;
 
-	cout << "La area es => " << vf_area << endl; 
+	// archivo funcion
+	//  bf_ area calcular la rais cuadrada de una variable e imprimir el  resultado
 
-	return 0;
+		return 0;
+	}
 }
