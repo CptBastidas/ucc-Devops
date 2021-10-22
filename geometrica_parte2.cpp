c353b3dfe765d6cacf833887cd87643b460bfc28
Juan Felipe Bastidas Salgado <Juan.felipe@campusucc.edu.co>
2021-10-21 20:33:45 -0500

    se particiona el archivo geometrica en dos parates que son geometrica_parte1.cpp y geometrica_parte2.cpp

---
+++ geometrica_parte2.cpp
@@ -0,0 +1,15 @@
+	    cout << "\n";
+            int argc
+            char const *argv[]
+        {
+            double numero= vf_area;
+            double RaizCuadrada = sqrt(vf_area);
+            cout << "la raiz cuadrada es => " << numero << RaizCuadrada << ;
+	    cout << "La area es => " << vf_area << endl;
+
+	// archivo funcion
+	//  bf_ area calcular la rais cuadrada de una variable e imprimir el  resultado
+
+		return 0;
+	}
+}
